e7a5aeaa0889549e5de39d10aa95b6948d0c4c68
Ingrid Rodriguez <ingrid.rodriguez@logicstudio.net>
2023-01-10 16:00:02 -0500

    Initial commit

---
+++ IngridtRodriguezPracticalTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IngridtRodriguezPracticalTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
